d5eb8dfa9c80971dd8fec5ff2b6a2699eac987a0
ptgbobby <45051440+ptgbobby@users.noreply.github.com>
2019-03-05 14:20:24 +0200

    Create landmarks.php

---
+++ landmarks.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="landmarks.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Забележителности във Враца | Vratsa - City Like a Balkan</title>
+    <link rel="icon" type="icon/png" href="logoicon.png">
+</head>
+<body>
+
+    <!--NAVIGATION BAR-->
+
+    <div class="bg">
+      <img src="img/bg/bg4.jpg">
+    </div>
+
+    <div class="dropdown">
+            <input type="checkbox" id="checkbox_toggle">
+            <label for="checkbox_toggle" class="hamburger">
+                <div class="line line-1"></div>
+                <div class="line line-2"></div>
+                <div class="line line-3"></div>
+             </label>
+            <nav>
+              <ul>
+              <li><a href="index.php">Начало</a></li>
+              <li><a href="contact.php">Контакти</a></li>
+              <li><a href="#landmarks">Забележителности</a></li>
+            </ul>
+              </nav>
+          </div>
+
+
+          <section id="landmarks">
+            <div class="textMain">
+              <h1>Забележителности във Враца</h1>
+
+              <p>Враца е град с голяма история...<br>
+              За щастие и до ден <span>днешен има доказателства за нея....</span><br>
+              Тя притежава както природни така и културни забележителности<br></p>
+
+
+              <p>Изберете какво търсите</p>
+              <a href="nature.php" class="btn1 wow fadeInLeft">
+                Природни забележителности
+              </a>
+
+              <a href="history.php" class="btn2 wow fadeInRight">
+                  Културни забележителности
+                </a>
+            </div>
+          </section>
+
+
+          <footer>
+            <div class="socialm">
+              <a href="#"><i class="fab fa-facebook socialm"></i></a>
+              <a href="#"><i class="fab fa-instagram socialm"></i></a>
+              <a href="#"><i class="fab fa-twitter-square socialm"></i></a>
+            </div>
+            <p class="mrg">vratsa-bg.com © 2018-2019</p>
+            <p class="mrg">Vratsa, Bulgaria</p>
+          </footer>
+          <div class="infoDevs">
+            <p class="txtDevs">Дизайн и разработка <a href="https://www.instagram.com/haji_bojo_whiskey/?hl=bg">Божидар Ангелов.</a> и <a href="https://www.instagram.com/vgenovonhorizon/?hl=bg">Валентин Генов</a></p>
+              <p class="txtDevs">Вижте още хора помогнали за сайта <a href="others.php">тук</a>.</p>
+            </div>
+
+            <script src="js/wow.min.js"></script>
+            <script>
+            new WOW().init();
+            </script>
+</body>
+</html>
