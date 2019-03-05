45f6dd2446432f12fa62830444f233d857c310e9
ptgbobby <45051440+ptgbobby@users.noreply.github.com>
2019-03-05 14:28:36 +0200

    ---ALL--FILES---

---
+++ landmarks.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133706682-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133706682-2');
+</script>
+  <meta name="keywords" content="Vratsa, vratsa, враца, Враца, Забележителности, Във, във, zabelejitelnosti, zabelezhitelnosti, nature, Природа">
+  <meta name="description" content="Най-хубавите забележителности във Враца">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="landmarks.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Забележителности във Враца | Vratsa - City Like a Balkan</title>
+    <link rel="icon" type="icon/png" href="logoicon.png">
+</head>
+<body>
+
+    <!--NAVIGATION BAR-->
+
+    <div class="bg">
+      <img src="img/bg/bg4.jpg">
+    </div>
+
+    <div class="dropdown">
+            <input type="checkbox" id="checkbox_toggle">
+            <label for="checkbox_toggle" class="hamburger">
+                <div class="line line-1"></div>
+                <div class="line line-2"></div>
+                <div class="line line-3"></div>
+             </label>
+            <nav>
+              <ul>
+              <li><a href="index.php">Начало</a></li>
+              <li><a href="contact.php">Контакти</a></li>
+              <li><a href="#landmarks">Забележителности</a></li>
+            </ul>
+              </nav>
+          </div>
+
+
+          <section id="landmarks">
+            <div class="textMain">
+              <h1>Забележителности във Враца</h1>
+
+              <p>Враца е град с голяма история...<br>
+              За щастие и до ден <span>днешен има доказателства за нея....</span><br>
+              Тя притежава както природни така и културни забележителности<br></p>
+
+
+              <p>Изберете какво търсите</p>
+              <a href="nature.php" class="btn1 wow fadeInLeft">
+                Природни забележителности
+              </a>
+
+              <a href="history.php" class="btn2 wow fadeInRight">
+                  Културни забележителности
+                </a>
+            </div>
+          </section>
+
+
+          <footer>
+            <div class="socialm">
+              <a href="#"><i class="fab fa-facebook socialm"></i></a>
+              <a href="#"><i class="fab fa-instagram socialm"></i></a>
+              <a href="#"><i class="fab fa-twitter-square socialm"></i></a>
+            </div>
+            <p class="mrg">vratsa-bg.com © 2018-2019</p>
+            <p class="mrg">Vratsa, Bulgaria</p>
+          </footer>
+          <div class="infoDevs">
+            <p class="txtDevs">Дизайн и разработка <a href="https://www.instagram.com/haji_bojo_whiskey/?hl=bg">Божидар Ангелов.</a> и <a href="https://www.instagram.com/vgenovonhorizon/?hl=bg">Валентин Генов</a></p>
+              <p class="txtDevs">Вижте още хора помогнали за сайта <a href="others.php">тук</a>.</p>
+            </div>
+
+            <script src="js/wow.min.js"></script>
+            <script>
+            new WOW().init();
+            </script>
+</body>
+</html>
---
+++ others.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133706682-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133706682-2');
+</script>
+  <meta name="keywords" content="Vratsa, vratsa, враца, Враца, Забележителности, Във, във, zabelejitelnosti, zabelezhitelnosti, nature, Природа">
+  <meta name="description" content="Най-хубавите забележителности във Враца">
+<link rel="icon" type="icon/png" href="logoicon.png">
+<link rel="stylesheet" type="icon/png" href="logoicon.png">
+  <link rel="stylesheet" type="icon/png" href="logoicon.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="others.css">
+    <title> Специални Благодарности | Vratsa - City Like the Balkan</title>
+</head>
+<body>
+    <!--NAVIGATION BAR-->
+
+    <div class="dropdown">
+        <input type="checkbox" id="checkbox_toggle">
+        <label for="checkbox_toggle" class="hamburger">
+            <div class="line line-1"></div>
+            <div class="line line-2"></div>
+            <div class="line line-3"></div>
+         </label>
+        <nav>
+          <ul>
+          <li><a href="index.php">Начало</a></li>
+          <li><a href="contact.php">Контакти</a></li>
+          <li><a href="landmarks.php" class="rght">Забележителности</a></li>
+        </ul>
+          </nav>
+      </div>
+
+      <!--CONTENT-->
+
+      <h1><span>Специални</span> Благодарности</h1>
+      <br><br>
+<img src="img/vrbalkan.jpg" style=" border-radius: 8px; width: 350px; "><br><img src="img/vuttre.jpg" style="margin-left: 0%; border-radius: 8px; width: 350px; "><br>
+<div class="vgenov">
+        <h3>Божидар Ангелов</h3>
+        <p>Програмист, фотограф, текст.</p>
+      </div>
+     
+
+      <i class="fas fa-ellipsis-h"></i>
+
+   <div class="vgenov">
+        <h3>Валентин Генов</h3>
+        <p>Дизайн.</p>
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+
+      <div class="vgenov">
+        <h3>Тодор Богданов</h3>
+        <p>Фотограф, редактор.</p>
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+
+      <div class="vgenov">
+        <h3 href="#">Веселин Дичков</h3>
+        <p>Фотограф.</p>
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+
+      <div class="vgenov">
+        <h3>Емилиян Кадийски</h3>
+        <p>Съосновател на Враца Софтуерно Общество, ментор, спонсор.</p>
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+
+      <div class="vgenov">
+        <h3 href="#">Весела Пелова</h3>
+        <p>Редактор.</p>
+      </div>
+ <i class="fas fa-ellipsis-h"></i>
+      <div class="vgenov">
+        <h3 href="#">Оля Генова</h3>
+        <p>Фотограф, редактор.</p>
+      </div>
+ <i class="fas fa-ellipsis-h"></i>
+  <div class="vgenov">
+        <h3 href="#">Румен Николов</h3>
+        <p>Редактор.</p>
+      </div>
+       <i class="fas fa-ellipsis-h"></i>
+       <div class="vgenov">
+        <h3 href="#">Михаил Кьосев</h3>
+        <p>Редактор.</p>
+      </div>
+
+ <i class="fas fa-ellipsis-h"></i>
+       <div class="vgenov">
+        <h3 href="#"><a href="https://superhosting.bg">SUPERHOSTING.BG</a></h3>
+        <p>Хостинг и домейн.</p>
+      </div>
+ <i class="fas fa-ellipsis-h"></i>
+      <div class="vgenov">
+        <h3 href="#"><a href="https://pixabay.com">Pixabay.com</a></h3>
+        <p>Снимки.</p>
+      </div>
+ <i class="fas fa-ellipsis-h"></i>
+
+</body>
+</html>
